f654c55c636b0a8fd2931fab0743d9f586f3e5f1
Mario Romero <marioromero.ttp@gmail.com>
2025-05-19 20:20:15 -0400

    Ajustes preliminares -> Comando de scaffold modificado para agregar trait de integridad al eliminar

--- Obi-Modules/app/Console/Commands/ModuleScaffold.php
+++ Obi-Modules/app/Console/Commands/ModuleScaffold.php
@@ -22,35 +22,68 @@ class ModuleScaffold extends Command
 
         // Sobrescribir rutas para que se generen donde deben
         config([
-            'modules.paths.generator.controller.path' => 'app/Http/Controllers',
-            'modules.paths.generator.request.path'    => 'app/Http/Requests',
-            'modules.paths.generator.resource.path'   => 'app/Transformers',
+            'modules.paths.generator.controller.path'      => 'app/Http/Controllers',
+            'modules.paths.generator.request.path'         => 'app/Http/Requests',
+            'modules.paths.generator.resource.path'        => 'app/Transformers',
             'modules.paths.generator.controller.namespace' => 'App\\Http\\Controllers',
         ]);
 
         $this->info("📦 Scaffold API para {$name} en módulo {$module}...");
 
-        // Modelo
+        //
+        // 1) Modelo + INYECCIÓN DE TRAIT
+        //
         $this->call('module:make-model', [
             'model'  => $name,
             'module' => $module,
         ]);
 
-        // Controlador API
+        // Ruta al archivo del modelo generado
+        $modelPath = base_path("Modules/{$module}/Models/{$name}.php");
+
+        if (file_exists($modelPath)) {
+            $text = file_get_contents($modelPath);
+
+            // 1.a) Añadir import del trait justo después del namespace
+            $text = str_replace(
+                "namespace Modules\\{$module}\\Models;",
+                "namespace Modules\\{$module}\\Models;\n\nuse Modules\\Core\\app\\Support\\Traits\\DeletionStrategies;",
+                $text
+            );
+
+            // 1.b) Insertar 'use DeletionStrategies;' dentro de la clase
+            //    buscamos la primera llave '{' de la clase y añadimos la línea
+            $text = preg_replace(
+                '/(class\s+' . preg_quote($name, '/') . '\s+extends\s+Model\s*\{)/',
+                "$1\n    use DeletionStrategies;\n",
+                $text,
+                1
+            );
+
+            file_put_contents($modelPath, $text);
+        }
+
+        //
+        // 2) Controlador API
+        //
         $this->call('module:make-controller', [
             'controller' => "{$name}Controller",
             '--api'      => true,
             'module'     => $module,
         ]);
 
-        // Migración
+        //
+        // 3) Migración
+        //
         $table = Str::plural(Str::snake($name));
         $this->call('module:make-migration', [
             'name'   => "create_{$table}_table",
             'module' => $module,
         ]);
 
-        // Requests
+        //
+        // 4) Requests
+        //
         $this->call('module:make-request', [
             'name'   => "Store{$name}Request",
             'module' => $module,
@@ -60,19 +93,25 @@ class ModuleScaffold extends Command
             'module' => $module,
         ]);
 
-        // Transformer (Resource)
+        //
+        // 5) Transformer (Resource)
+        //
         $this->call('module:make-resource', [
             'name'   => "{$name}Resource",
             'module' => $module,
         ]);
 
-        // Factory
+        //
+        // 6) Factory
+        //
         $this->call('module:make-factory', [
             'name'   => "{$name}Factory",
             'module' => $module,
         ]);
 
-        // Seeder
+        //
+        // 7) Seeder
+        //
         $this->call('module:make-seed', [
             'name'   => "{$name}Seeder",
             'module' => $module,
