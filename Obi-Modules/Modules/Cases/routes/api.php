4202eeea4c500df3c4397c123533969bfcffe644
Kazorhx <pablo.ortiz@cftsa.cl>
2025-08-23 21:35:29 -0400

    api.php de cases despues del merge

--- Obi-Modules/Modules/Cases/routes/api.php
+++ Obi-Modules/Modules/Cases/routes/api.php
@@ -2,13 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 use Modules\Cases\app\Http\Controllers\CaseDocumentController;
-
+use Modules\Cases\app\Http\Controllers\AccidentTypeController;
+use Modules\Cases\app\Http\Controllers\CaseStatusController;
+use Modules\Cases\app\Http\Controllers\CommentController;
+use Modules\Cases\app\Http\Controllers\PriorityController;
+use Modules\Cases\app\Http\Controllers\CaseController;
+use Modules\Cases\app\Http\Controllers\CaseSubstateController;
+use Modules\Cases\app\Http\Controllers\AgreementController;
 
 Route::get('/ping-cases', fn() => response()->json(['pong' => 'Cases']))->name('Cases.ping');
 
 
 // REST para AccidentType
-use Modules\Cases\app\Http\Controllers\AccidentTypeController;
 Route::get('accident-types', [AccidentTypeController::class, 'index']);
 Route::post('accident-types', [AccidentTypeController::class, 'store']);
 Route::get('accident-types/{accidentType}', [AccidentTypeController::class, 'show'])->whereNumber('accidentType');
@@ -18,7 +23,6 @@ Route::delete('accident-types/{accidentType}', [AccidentTypeController::class, '
 
 
 // REST para CaseStatus
-use Modules\Cases\app\Http\Controllers\CaseStatusController;
 Route::get('case-statuses', [CaseStatusController::class, 'index']);
 Route::get('case-statuses/{caseStatus}', [CaseStatusController::class, 'show']);
 Route::post('case-statuses', [CaseStatusController::class, 'store']);
@@ -27,7 +31,6 @@ Route::patch('case-statuses/{caseStatus}', [CaseStatusController::class, 'patch'
 Route::delete('case-statuses/{caseStatus}', [CaseStatusController::class, 'destroy']);
 
 // REST para Comment
-use Modules\Cases\app\Http\Controllers\CommentController;
 Route::get('comments', [CommentController::class, 'index']);
 Route::get('comments/{comment}', [CommentController::class, 'show']);
 Route::post('comments', [CommentController::class, 'store']);
@@ -36,7 +39,6 @@ Route::patch('comments/{comment}', [CommentController::class, 'patch']);
 Route::delete('comments/{comment}', [CommentController::class, 'destroy']);
 
 // REST para Priority
-use Modules\Cases\app\Http\Controllers\PriorityController;
 Route::get('priorities', [PriorityController::class, 'index']);
 Route::get('priorities/{priority}', [PriorityController::class, 'show']);
 Route::post('priorities', [PriorityController::class, 'store']);
@@ -45,8 +47,6 @@ Route::patch('priorities/{priority}', [PriorityController::class, 'patch']);
 Route::delete('priorities/{priority}', [PriorityController::class, 'destroy']);
 
 // REST para Cases
-use Modules\Cases\app\Http\Controllers\CaseController;
-use Modules\Cases\app\Http\Controllers\CaseSubstateController;
 Route::get('cases/stats', [CaseController::class, 'stats']);
 Route::get('cases', [CaseController::class, 'index']);
 Route::post('cases', [CaseController::class, 'store']);
@@ -62,7 +62,6 @@ Route::post('cases/{case}/transition', [CaseController::class, 'transition'])->w
 Route::post('cases/{case}/substate', [CaseSubstateController::class, 'update'])->whereNumber('case');
 
 // REST para Agreement
-use Modules\Cases\app\Http\Controllers\AgreementController;
 Route::get('agreements', [AgreementController::class, 'index']);
 Route::get('agreements/{agreement}', [AgreementController::class, 'show']);
 Route::post('agreements', [AgreementController::class, 'store']);
