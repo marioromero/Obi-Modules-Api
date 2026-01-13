c4a35681d7941ee088e31ec38a2aa425f5dd7731
Kazorhx <pablo.ortiz@cftsa.cl>
2026-01-13 15:40:05 -0300

    ajuste estructuracion tabla email schedule

--- Obi-Modules/Modules/Mailing/database/migrations/2025_04_23_204920_create_email_schedules_table.php
+++ Obi-Modules/Modules/Mailing/database/migrations/2025_04_23_204920_create_email_schedules_table.php
@@ -14,10 +14,17 @@ return new class extends Migration
             $table->id();
             $table->dateTime('start_in');
             $table->dateTime('ending_at');
-            $table->integer('send_once'); // 25 seg es el intervalo de envio
+            $table->integer('send_once'); // 25 seg es el intervalo de envio de cada correo
+            $table->integer('sends_ok')->default(0);
             $table->integer('failed_or_pendings')->default(0);
             $table->boolean('is_retry')->default(false);
+            $table->string('status', 32)->default('pending'); // pending, in_progress, paused, finished, canceled
+            $table->boolean('only_business_days')->default(false);
+            $table->unsignedBigInteger('user_id'); // creador (Auth o payload)
             $table->unsignedBigInteger('customer_set_id');
+            $table->unsignedBigInteger('email_template_id');
+
+            $table->timestamps();
         });
     }
 
