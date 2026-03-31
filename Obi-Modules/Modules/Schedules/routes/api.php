6b7bdf6449db16770f8a3669d908e5c50127c863
Mario Romero <marioromero.ttp@gmail.com>
2026-03-31 18:25:31 -0300

    feat: rutas ok

--- Obi-Modules/Modules/Schedules/routes/api.php
+++ Obi-Modules/Modules/Schedules/routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Schedules\app\Http\Controllers\ScheduleController;
 use Modules\Schedules\app\Http\Controllers\ScheduleStatusController;
+use Modules\Schedules\app\Http\Controllers\DispatchController;
+use Modules\Schedules\app\Http\Controllers\DispatchDetailController;
 
 Route::get('/ping-schedules', fn() => response()->json(['pong' => 'Schedules']))->name('Schedules.ping');
 
@@ -25,4 +27,24 @@ Route::post('schedule-statuses', [ScheduleStatusController::class, 'store']);
 Route::put('schedule-statuses/{scheduleStatus}', [ScheduleStatusController::class, 'update']);
 Route::patch('schedule-statuses/{scheduleStatus}', [ScheduleStatusController::class, 'patch']);
 Route::delete('schedule-statuses/{scheduleStatus}', [ScheduleStatusController::class, 'destroy']);
+
+// REST para Dispatch
+Route::get('dispatches', [DispatchController::class, 'index']);
+Route::get('dispatches/{dispatch}', [DispatchController::class, 'show']);
+Route::post('dispatches', [DispatchController::class, 'store']);
+Route::put('dispatches/{dispatch}', [DispatchController::class, 'update']);
+Route::patch('dispatches/{dispatch}', [DispatchController::class, 'patch']);
+Route::delete('dispatches/{dispatch}', [DispatchController::class, 'destroy']);
+Route::get('dispatches/date/{date}', [DispatchController::class, 'indexByDate']);           // Listar despachos por fecha
+Route::get('dispatches/{dispatch}/details', [DispatchController::class, 'showWithDetails']); // Despacho con detalles
+
+// REST para DispatchDetail
+Route::get('dispatch-details', [DispatchDetailController::class, 'index']);
+Route::get('dispatch-details/{dispatchDetail}', [DispatchDetailController::class, 'show']);
+Route::post('dispatch-details', [DispatchDetailController::class, 'store']);
+Route::put('dispatch-details/{dispatchDetail}', [DispatchDetailController::class, 'update']);
+Route::patch('dispatch-details/{dispatchDetail}', [DispatchDetailController::class, 'patch']);
+Route::delete('dispatch-details/{dispatchDetail}', [DispatchDetailController::class, 'destroy']);
+Route::get('dispatch-details/dispatch/{dispatchId}', [DispatchDetailController::class, 'indexByDispatchId']);           // Listar detalles por despacho
+Route::get('dispatch-details/{dispatchDetail}/movement-type', [DispatchDetailController::class, 'showWithMovementType']); // Detalle con tipo de movimiento
 
