f4b05bac6d0e7139320b22a3e0a3107d999e371a
Mario Romero <marioromero.ttp@gmail.com>
2026-04-01 21:39:20 -0300

    fix: error de mailing (resquicio)

--- Obi-Modules/Modules/Mailing/Providers/MailingServiceProvider.php
+++ Obi-Modules/Modules/Mailing/Providers/MailingServiceProvider.php
@@ -46,10 +46,6 @@ class MailingServiceProvider extends ServiceProvider
         if (! $this->app->runningInConsole()) {
         return;
         }
-    
-        $this->commands([
-            \Modules\Mailing\Console\Commands\RunMailingSchedulesCommand::class,
-        ]);
     }
 
     /**
